ff3c46429fa6e5ecb982b5944bf94a3b2ab33c4b
SAHIL RAJA <sraza1692@gmail.com>
2025-09-03 00:48:55 +0530

    Add files via upload

---
+++ RecursionBasics.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class RecursionBasics{
+    public static int firstOccurance(int arr[], int key , int i){
+        // base case
+        if(i == arr.length){
+            return -1;
+        }
+        if(arr[i] == key){
+            return i;
+        }
+        return firstOccurance(arr, key, i+1);
+    }
+    public static void main(String args[]){
+    int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 3};
+    System.out.println(firstOccurance(arr, 5, 0)); 
+    }
+}
